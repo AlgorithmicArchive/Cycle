03075f3e9ba2fec321991b00d9bf0ef911f198f0
Momin <AlgorithmicArchive@gmail.com>
2025-01-14 09:38:30 +0530

    Moved out

--- Server/routes/Userroutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { GetLastRecord } from "../controllers/User/GetData.js";
-
-const router = Router();
-
-router.get("/lastrecord", GetLastRecord);
-
-export default router;
